a8f11cf3f310cd973fc120c2de6594eab8264d85
Ruslana Melnik <rusalna0417@gmail.com>
2023-09-25 18:11:04 +0300

    Interface for click workout list events

---
+++ app/src/main/java/com/example/sportbook/Interfaces/WorkoutClickListener.java
@@ -0,0 +1,8 @@
+package com.example.sportbook.Interfaces;
+
+import android.view.View;
+
+public interface WorkoutClickListener {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
